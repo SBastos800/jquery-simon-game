d477f07a29a91ac20b8b4bfaf0f2b6a9c84f7359
SBastos800 <soniavsbastos@gmail.com>
2020-08-24 18:33:57 +0100

    function to play the sounds correspondent to the colors

--- game.js
+++ game.js
@@ -7,6 +7,8 @@ let userClickedPattern = [];
 $(".btn").click(function() {
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
 })
 
 const nextSequence = () => {
@@ -15,12 +17,14 @@ const nextSequence = () => {
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    //play a sound for the button color selected
-    const audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    
+    playSound(randomChosenColour);
 }
 
+const playSound = (name) => {
+    const audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 
 // $('#green').click(() => {
 //     const audio = new Audio("sounds/green.mp3");

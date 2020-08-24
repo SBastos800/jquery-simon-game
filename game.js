63720d3c47702169030b4b904c915b4690cb30a7
SBastos800 <soniavsbastos@gmail.com>
2020-08-24 18:27:01 +0100

    function to identify to button cliked

--- game.js
+++ game.js
@@ -2,6 +2,13 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 
+let userClickedPattern = [];
+
+$(".btn").click(function() {
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+})
+
 const nextSequence = () => {
     const randomNumber = Math.floor((Math.random() * 3) + 1);
     const randomChosenColour = buttonColours[randomNumber];
@@ -12,15 +19,14 @@ const nextSequence = () => {
     //play a sound for the button color selected
     const audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
-
-
 }
 
-$('#green').click(() => {
-    const audio = new Audio("sounds/green.mp3");
-    audio.play();
-  });
 
-nextSequence();
+// $('#green').click(() => {
+//     const audio = new Audio("sounds/green.mp3");
+//     audio.play();
+//   });
+
+
 
 

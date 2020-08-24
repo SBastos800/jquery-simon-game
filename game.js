8b3ae48cd1e73c36f92feb819542eaab06ec2a38
SBastos800 <soniavsbastos@gmail.com>
2020-08-24 17:13:58 +0100

    generate a random color

--- game.js
+++ game.js
@@ -1,2 +1,12 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
 
+let gamePattern = [];
+
+const nextSequence = () => {
+    const randomNumber = Math.floor((Math.random() * 3) + 1);
+    const randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+
+}
 
